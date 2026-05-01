97a1b5fb576e8f1c20079fad8b58ce9299501d18
Samprit <samprit235@gmail.com>
2026-05-01 19:13:49 +0530

    fix Express 5 wildcard syntax

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use('/api/memories', memoryRoutes);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../dist')));
 
-  app.get('*', (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
   });
 }
